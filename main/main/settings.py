d5176ae24832d7f62617fcd4a8d78066880020fc
AliGhalenoei <alighalenoei8383@gmail.com>
2024-05-05 20:57:16 +0330

    edit project

--- main/main/settings.py
+++ main/main/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'vod_project',
         'USER': 'vod',
         'PASSWORD': 'vod-project',
